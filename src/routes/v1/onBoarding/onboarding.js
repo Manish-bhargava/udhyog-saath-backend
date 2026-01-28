a1e8a71977c6e43cf823eaf349fbdb141f4afdc7
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2026-01-29 01:25:45 +0530

    added the multer middleware and also integrated the uploadImage functions in the onboarding POST api

--- src/routes/v1/onBoarding/onboarding.js
+++ src/routes/v1/onBoarding/onboarding.js
@@ -1,8 +1,19 @@
-const express=require("express");
-const {userOnboarding}=require("../../../controllers/onboarding/user.onboarding");
-const {getUserProfile}=require("../../../controllers/user-auth/getProfile");
-const verify=require("../../../utils/auth");
-const onBoardingRouter=express.Router();
-onBoardingRouter.post('/onboarding',verify,userOnboarding);
-onBoardingRouter.get("/profile", verify, getUserProfile );
-module.exports=onBoardingRouter;
+const express = require("express");
+const {
+  userOnboarding,
+} = require("../../../controllers/onboarding/user.onboarding");
+const { getUserProfile } = require("../../../controllers/user-auth/getProfile");
+const { uploads } = require("../../../utils/multer");
+const verify = require("../../../utils/auth");
+const onBoardingRouter = express.Router();
+onBoardingRouter.post(
+  "/onboarding",
+  verify,
+  uploads.fields([
+    { name: "companyLogo", maxCount: 1 },
+    { name: "companySignature", maxCount: 1 },
+  ]),
+  userOnboarding,
+);
+onBoardingRouter.get("/profile", verify, getUserProfile);
+module.exports = onBoardingRouter;
