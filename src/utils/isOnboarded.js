0e2600e90b274a26791731af7f43ab04e65846f3
Debarshi Majumder <debarshimajumder.dev@gmail.com>
2026-03-05 11:55:55 +0530

    fixed file upload bug

--- src/utils/isOnboarded.js
+++ src/utils/isOnboarded.js
@@ -7,27 +7,6 @@ const isOnboarded = async (req, res, next) => {
     if (!userId) {
       return res.status(401).json({ success: false, message: "Unauthorized: No user found" });
     }
-
-   
-   
-   
-
-      
-    if (cachedStatus !== null) {
-       console.log("cache hit");
-      const isComplete = cachedStatus === "true";
-
-      if (!isComplete) {
-        return res.status(403).json({
-          success: false,
-          message: "⛔ Access Denied: onboarding required (Cache)",
-          error_code: "WORKSPACE_LOCKED"
-        });
-      }
-      
-      return next();
-    }
-       console.log("cache miss");
              
     const user = await User.findById(userId);
       
@@ -35,8 +14,6 @@ const isOnboarded = async (req, res, next) => {
         return res.status(404).json({ success: false, message: "User not found in Database" });
     }
 
-
-            
     if (!user.onboarding) {
       return res.status(403).json({
         success: false,
@@ -49,14 +26,14 @@ const isOnboarded = async (req, res, next) => {
 
   } catch (error) {
 
-    try {
-        const userId = req.user?._id || req.user?.id;
-        const user = await User.findById(userId).select("onboarding");
+    // try {
+    //     const userId = req.user?._id || req.user?.id;
+    //     const user = await User.findById(userId).select("onboarding");
      
-        if (user?.onboarding) return next();
-    } catch (dbErr) {
-        return res.status(500).json({ success: false, message: "Internal Server Error" });
-    }
+    //     if (user?.onboarding) return next();
+    // } catch (dbErr) {
+    //     return res.status(500).json({ success: false, message: "Internal Server Error" });
+    // }
     
     res.status(500).json({ success: false, message: "Server Error in Auth Check" });
   }
